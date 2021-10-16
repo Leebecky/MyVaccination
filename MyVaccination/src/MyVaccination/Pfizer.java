515b243807e1bdcffb24f849bf14d58271db3611
Leebecky <leebecky06@gmail.com>
2021-10-16 13:09:29 +0800

    MyVaccination Classes
    - added AstraZeneca and Pfizer as children of Vaccine
    - updated Vaccine class

---
+++ MyVaccination/src/MyVaccination/Pfizer.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MyVaccination;
+
+import java.time.Period;
+import java.util.UUID;
+
+/**
+ *
+ * @author leebe
+ */
+public class Pfizer extends Vaccine {
+
+    public Pfizer() {
+        this.manufacturer = "Pfizer";
+        this.name = "Pfizer";
+        this.waitTime = Period.ofWeeks(3);
+        this.amount = 50;
+        this.dosage = 5;
+        this.batchNumber = "PF_" + UUID.randomUUID().toString();
+        this.dosesRequired = 2;
+    }
+
+}
